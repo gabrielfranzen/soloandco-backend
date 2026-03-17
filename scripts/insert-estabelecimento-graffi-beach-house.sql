0b6f46ebc7d46905e2824cad35068fea18140496
gabrielfranzen <gabrielreisfranzen@gmail.com>
2026-03-16 22:22:52 -0300

    insert graffi

---
+++ scripts/insert-estabelecimento-graffi-beach-house.sql
@@ -0,0 +1,76 @@
+BEGIN;
+
+DO $$
+DECLARE
+    v_usuario_id INTEGER;
+    v_papel_empresario_id INTEGER;
+    v_estabelecimento_id INTEGER;
+BEGIN
+    SELECT u.id
+      INTO v_usuario_id
+      FROM website.usuario u
+     WHERE u.email = 'gfranzen005@gmail.com';
+
+    IF v_usuario_id IS NULL THEN
+        RAISE EXCEPTION 'Usuário com e-mail % não foi encontrado.', 'gfranzen005@gmail.com';
+    END IF;
+
+    INSERT INTO website.papeis (id, nome, codigo)
+    SELECT nextval('website.seq_papeis'), 'Empresário', 'empresario'
+    WHERE NOT EXISTS (
+        SELECT 1
+          FROM website.papeis
+         WHERE codigo = 'empresario'
+    );
+
+    SELECT p.id
+      INTO v_papel_empresario_id
+      FROM website.papeis p
+     WHERE p.codigo = 'empresario';
+
+    INSERT INTO website.usuario_papel (usuario_id, papel_id)
+    SELECT v_usuario_id, v_papel_empresario_id
+    WHERE NOT EXISTS (
+        SELECT 1
+          FROM website.usuario_papel up
+         WHERE up.usuario_id = v_usuario_id
+           AND up.papel_id = v_papel_empresario_id
+    );
+
+    SELECT e.id
+      INTO v_estabelecimento_id
+      FROM website.estabelecimento e
+     WHERE e.nome = 'Graffi Beach House - Hospedagem, Restaurante e Festas'
+       AND e.endereco = 'R. Aroeira do Campo, 25B - Campeche, Florianópolis - SC, 88066-280';
+
+    IF v_estabelecimento_id IS NULL THEN
+        INSERT INTO website.estabelecimento (
+            nome,
+            latitude,
+            longitude,
+            endereco,
+            ativo,
+            usuario_proprietario_id
+        )
+        VALUES (
+            'Graffi Beach House - Hospedagem, Restaurante e Festas',
+            -27.706228,
+            -48.497777,
+            'R. Aroeira do Campo, 25B - Campeche, Florianópolis - SC, 88066-280',
+            TRUE,
+            v_usuario_id
+        )
+        RETURNING id INTO v_estabelecimento_id;
+    ELSE
+        UPDATE website.estabelecimento
+           SET latitude = -27.706228,
+               longitude = -48.497777,
+               ativo = TRUE,
+               usuario_proprietario_id = v_usuario_id
+         WHERE id = v_estabelecimento_id;
+    END IF;
+
+    RAISE NOTICE 'Estabelecimento processado com sucesso. ID: %', v_estabelecimento_id;
+END $$;
+
+COMMIT;
