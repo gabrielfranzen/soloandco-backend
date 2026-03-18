6dba6b18de74dfba491019fcf04aa6801c25d592
gabrielfranzen <gabrielreisfranzen@gmail.com>
2026-03-17 23:56:00 -0300

    mais melhorias para o mvp

--- src/main/java/model/dto/EventoLinkRequest.java
+++ src/main/java/model/dto/EventoLinkRequest.java
@@ -1,5 +1,8 @@
 package model.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EventoLinkRequest {
 
     private String titulo;
--- src/main/java/model/dto/EventoRequest.java
+++ src/main/java/model/dto/EventoRequest.java
@@ -2,6 +2,9 @@ package model.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EventoRequest {
 
     private String nome;
